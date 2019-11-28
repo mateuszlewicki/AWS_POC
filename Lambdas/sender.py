0ff71c5b11ce9311b6ac085dca8777f95765c433
Mateusz Lewicki <mateusz.lewicki@windowslive.com>
2019-11-28 04:37:29 +0000

    Update Lambdas/sender.py

--- Lambdas/sender.py
+++ Lambdas/sender.py
@@ -1,2 +1,29 @@
 from http import HTTPStatus
 import boto3, json, psycopg2
+
+def lambda_handler(event, context):
+    # return checkSystem(event["system"])=="MTT"?noSQL_MTT(event):noSQL_Website(event)
+    client = boto3.client('ses')
+    response = client.send_email(
+        Source='string',
+        Destination={
+            'ToAddresses': [
+                'mateusz.lewicki@atos.net',
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': 'New Package to you',
+                'Charset': 'UTF-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': "New package was sent to you \n Package number: "+str(event['pkgnum']),
+                    'Charset': 'UTF-8'
+                }
+            }
+        },
+        ReplyToAddresses=[
+            'mateusz.lewicki@windowslive.com',
+        ],
+    )
