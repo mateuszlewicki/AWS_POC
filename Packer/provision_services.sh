f36f37e8101ac67c4ca4d35d6c7b5c9ab319a122
Mateusz Lewicki <mateusz.lewicki@windowslive.com>
2019-11-29 11:06:05 +0000

    Deleted Packer/provision_services.sh, Packer/worker_services.sh files

--- Packer/provision_services.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo systemctl enable vault && sudo systemctl enable consul && sudo systemctl enable fabio &&  sudo systemctl enable nomad
--- Packer/worker_services.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo systemctl enable consul && sudo systemctl enable nomad
