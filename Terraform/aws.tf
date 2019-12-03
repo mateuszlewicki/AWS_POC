3bd6b8d785c176f339770a0949031641173a3704
Mateusz Lewicki (work) <mateusz.lewicki@windowslive.com>
2019-12-03 12:55:11 +0100

    FIRE!!!

--- Terraform/aws.tf
+++ Terraform/aws.tf
@@ -341,57 +341,28 @@ resource "aws_lambda_function" "delete"{
 # API Endpoint
 
 resource "aws_api_gateway_rest_api" "graphQl_intake" {
-  name        = "ServerlessExample"
-  description = "Terraform Serverless Application Example"
+  name        = "graphQl_intake"
 }
 
 
-//  resource "aws_api_gateway_resource" "proxy" {
-//    rest_api_id = aws_api_gateway_rest_api.graphQl_intake.id
-//    parent_id   = aws_api_gateway_rest_api.graphQl_intake.root_resource_id
-//    path_part   = "{proxy+}"
-// }
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "resource"
+  parent_id   = "${aws_api_gateway_rest_api.graphQl_intake.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.graphQl_intake.id}"
+}
 
-// resource "aws_api_gateway_method" "proxy" {
-//    rest_api_id   = aws_api_gateway_rest_api.graphQl_intake.id
-//    resource_id   = aws_api_gateway_resource.graphQl_intake.id
-//    http_method   = "ANY"
-//    authorization = "NONE"
-//  }
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.graphQl_intake.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
 
-//  resource "aws_api_gateway_integration" "lambda" {
-//    rest_api_id = aws_api_gateway_rest_api.graphQl_intake.id
-//    resource_id = aws_api_gateway_method.proxy.resource_id
-//    http_method = aws_api_gateway_method.proxy.http_method
-
-//    integration_http_method = "POST"
-//    type                    = "AWS_PROXY"
-//    uri                     = aws_lambda_function.aws_poc_handler.invoke_arn
-//  }
-
-//   resource "aws_api_gateway_method" "proxy_root" {
-//    rest_api_id   = aws_api_gateway_rest_api.graphQl_intake.id
-//    resource_id   = aws_api_gateway_rest_api.graphQl_intake.root_resource_id
-//    http_method   = "ANY"
-//    authorization = "NONE"
-//  }
-
-//  resource "aws_api_gateway_integration" "lambda_root" {
-//    rest_api_id = aws_api_gateway_rest_api.graphQl_intake.id
-//    resource_id = aws_api_gateway_method.proxy_root.resource_id
-//    http_method = aws_api_gateway_method.proxy_root.http_method
-
-//    integration_http_method = "POST"
-//    type                    = "AWS_PROXY"
-//    uri                     = aws_lambda_function.aws_poc_handler.invoke_arn
-//  }
-
-// resource "aws_api_gateway_deployment" "example" {
-//    depends_on = [
-//      aws_api_gateway_integration.lambda,
-//      aws_api_gateway_integration.lambda_root,
-//    ]
-
-//    rest_api_id = aws_api_gateway_rest_api.example.id
-//    stage_name  = "test"
-//  }
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.graphQl_intake.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.handler.invoke_arn}"
+}
